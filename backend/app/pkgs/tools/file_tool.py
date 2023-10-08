07fd4e5bb74511badd8f0fc292a671467b57b557
booboosui <139328168+booboosui@users.noreply.github.com>
2023-10-08 16:07:09 +0800

    Update file_tool.py

--- backend/app/pkgs/tools/file_tool.py
+++ backend/app/pkgs/tools/file_tool.py
@@ -1,10 +1,9 @@
 import os
-
+import logging
 from config import WORKSPACE_PATH
 
-
 def read_file_content(filename):
-    print("read_file_content:" + filename)
+    logging.info("read_file_content: %s", filename)
     try:
         with open(filename, 'r', encoding='utf-8') as f:
             content = f.read()
@@ -14,14 +13,17 @@ def read_file_content(filename):
         result = False
     return result, content
 
-
 def write_file_content(filename, content):
-    print("write_file_content:" + filename)
+    logging.info("write_file_content: %s", filename)
     directory = os.path.dirname(filename)
     os.makedirs(directory, exist_ok=True)
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(content)
-
+    try:
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(content)
+            result = True
+    except PermissionError:
+        result = False
+    return result
 
 def get_ws_path(task_id):
-    return WORKSPACE_PATH + task_id
+    return os.path.join(WORKSPACE_PATH, task_id)
